3a404c769f51aeb02d05befb0eca52f24ce30131
liuyinping <1689372042@qq.com>
2019-08-21 11:29:49 +0800

    modify

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,19 +1,16 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-	var c = new Array();
-  var collectionB=objectB.value;
-  for(var i = 0; i<collectionA.length ; i++)
-  {
-    for(var j = 0; j<collectionB.length ; j++)
-    {
-      if (collectionA[i] == collectionB[j])
-      {
-        c.push(collectionA[i]);
-        break;
+  var sameElements=[];
+  var newCollectionB=objectB.value;
+  for (var i=0;i<collectionA.length;i++){
+    for (var j=0;j<newCollectionB.length;j++){
+      if (collectionA[i]===newCollectionB[j]){
+        sameElements.push(collectionA[i]);
       }
-    }
+    }   
   }
-  return c;
+
+  return sameElements;
   //return '实现练习要求，并改写该行代码。';
 }
--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,21 +1,15 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-	var c = new Array(); 
-    var collectionB=objectB.value;
-    for(var i = 0; i<collectionA.length ; i++)
-    {
-      for(var j = 0; j<collectionB.length ; j++)
-      {
-        if (collectionA[i].key == collectionB[j]) //取得对象数组中对象的key值
-        {
-          c.push(collectionB[j]);
-          break;
-        }
+    var sameElements=[];
+    //var newCollectionA=[];
+    for (var i=0;i<collectionA.length;i++){
+      for (var j=0;j<objectB.value.length;j++){
+     // newCollectionA.push(collectionA[i].key);
+    if (collectionA[i].key==objectB.value[j]){
+      sameElements.push(collectionA[i].key)
+    }
       }
     }
-    return c;
-	
-	
-  //return '实现练习要求，并改写该行代码。';
+    return sameElements;
 }
